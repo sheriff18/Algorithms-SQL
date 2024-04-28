3c073e54b0c951706f2755b25206b7c5e2a3d696
sheriff18 <135992427+sheriff18@users.noreply.github.com>
2024-04-28 12:29:13 -0400

    Time: 2795 ms (11.33%) | Memory: 0B (100.00%) - LeetSync

---
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -0,0 +1,13 @@
+WITH cte AS (
+    SELECT e.name AS employee_name, 
+           d.name AS department_name, 
+           salary,
+           DENSE_RANK() OVER (PARTITION BY d.id ORDER BY salary desc) AS rn
+    FROM employee e
+    LEFT JOIN department d ON d.id = e.departmentID AND d.name != e.name
+)
+SELECT  department_name AS Department,employee_name AS Employee, 
+       salary
+FROM cte 
+WHERE rn <= 3
+ORDER BY salary desc;
