7224e52993369c434bd4b2833b666ac96effe017
sheriff18 <135992427+sheriff18@users.noreply.github.com>
2024-05-16 15:05:47 -0400

    Time: 46 ms (78.17%) | Memory: 17.4 MB (50.42%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.py
@@ -0,0 +1,9 @@
+import bisect
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        return bisect.bisect_left(nums, target )
+       
+
+        
+            
+        
