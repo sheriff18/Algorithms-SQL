4cb072f97978698ce41ffa55bbf3544f313ec599
sheriff18 <135992427+sheriff18@users.noreply.github.com>
2024-05-20 15:55:37 -0400

    Time: 1211 ms (89.22%) | Memory: 55.3 MB (54.09%) - LeetSync

---
+++ 204-count-primes/count-primes.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n <= 2:
+            return 0
+
+        isPrime = [True]*n
+        isPrime[0] = isPrime[1] = False
+
+        for i in range(2,int(n**0.5)+1):
+            if isPrime[i]:
+                for j in range(i*i,n,i):
+                    isPrime[j] = False
+        return sum(isPrime)
+         
+
+        
