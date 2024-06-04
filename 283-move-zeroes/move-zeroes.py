665c3d82ce69175c5423e56f0e43889ee7081af3
sheriff18 <135992427+sheriff18@users.noreply.github.com>
2024-06-03 23:43:15 -0400

    Time: 115 ms (95.45%) | Memory: 18 MB (72.68%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -1,18 +1,39 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        arr = []
+        n = len(nums)
+        a = nums
+        j = -1
+    # Place the pointer j
+        for i in range(n):
+            if a[i] == 0:
+                j = i
+                break
+    
+    # No non-zero elements
+        if j == -1:
+            return a
+    
+    # Move the pointers i and j and swap accordingly
+        for i in range(j + 1, n):
+
+            if a[i] != 0:
+                a[i], a[j] = a[j], a[i]
+                j += 1
+    
+        return a
+        # j = -1
+        # n = len(nums)
+        # for i in range(n):
+        #     if nums[i] == 0:
+        #         j = i
+        #         break 
         
-        for num in nums:
-            if num != 0:
-                arr.append(num)
-        
-        for i in range(len(arr)):
-            nums[i] = arr[i]
-            
-        for i in range(len(arr),len(nums)):
-            nums[i] = 0
-        
-        return nums
+        # for i in range(j+1,n):
+        #     if nums[i] != 0:
+        #         nums[i], nums[j] = nums[j], nums[i]
+        #         j += 1
+
+        # return nums
 
         """
         Do not return anything, modify nums in-place instead.
