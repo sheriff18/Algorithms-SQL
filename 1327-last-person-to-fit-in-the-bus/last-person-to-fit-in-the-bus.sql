3ffdc367b25dbbc411d282c2540dc228e0e47169
sheriff18 <135992427+sheriff18@users.noreply.github.com>
2024-06-05 18:38:38 -0400

    Time: 767 ms (100.00%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+with cte as (
+    select *, sum(weight) over (order by turn) as cum_sum
+    from queue 
+    
+)
+
+select 
+    person_name
+from cte
+where cum_sum <= 1000
+order by cum_sum desc
+limit 1
