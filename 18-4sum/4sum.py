c304f8d034164c4c9bf635dbfe086e76601b9f98
sheriff18 <135992427+sheriff18@users.noreply.github.com>
2024-06-12 03:14:48 -0400

    Time: 418 ms (70.98%) | Memory: 16.3 MB (99.20%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -6,35 +6,32 @@ class Solution:
         nums.sort()
 
         for i in range(n):
-            if i > 0 and nums[i] == nums[i - 1]:
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
             
-            for j in range(i + 1, n):
-                if j > i + 1 and nums[j] == nums[j - 1]:
+            for j in range(i+1,n):
+                if j > i+1 and nums[j] == nums[j-1]:
                     continue
-                
-                k = j + 1
-                l = n - 1
+            
+                k, l = j+1, n-1
 
                 while k < l:
                     soln = nums[i] + nums[j] + nums[k] + nums[l]
-
                     if soln > target:
                         l -= 1
                     elif soln < target:
                         k += 1
                     else:
-                        arr.append([nums[i], nums[j], nums[k], nums[l]])
+                        arr.append([nums[i],nums[j],nums[k],nums[l]])
                         k += 1
                         l -= 1
 
-                        # Skip duplicates for k and l
-                        while k < l and nums[k] == nums[k - 1]:
+                        while k < l and nums[k] == nums[k-1]:
                             k += 1
-                        while k < l and nums[l] == nums[l + 1]:
+                        while k < l and nums[l] == nums[l+1]:
                             l -= 1
 
-                        
+            
                 
 
         return arr
