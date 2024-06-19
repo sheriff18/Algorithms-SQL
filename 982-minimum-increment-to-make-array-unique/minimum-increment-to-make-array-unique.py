15f6f50c7aacf1615c40edf6e588d3da524a5186
sheriff18 <135992427+sheriff18@users.noreply.github.com>
2024-06-19 12:38:14 -0400

    Time: 614 ms (79.47%) | Memory: 30.8 MB (37.23%) - LeetSync

---
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minIncrementForUnique(self, nums: List[int]) -> int:
+        n = len(nums)
+        nums.sort()
+
+        moves, expected = 0,min(nums)
+
+        for i in range(n):
+            if nums[i] < expected:
+                moves += expected - nums[i]
+            else:
+                expected = nums[i]
+            expected += 1
+        
+        return moves
+
+        
