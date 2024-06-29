67ce157181887327a977a67433980d0767ef3816
sheriff18 <135992427+sheriff18@users.noreply.github.com>
2024-06-29 11:37:53 -0400

    Time: 396 ms (7.16%) | Memory: 17 MB (12.88%) - LeetSync

---
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numberOfSubstrings(self, s: str) -> int:
+        cnt = 0
+        n = len(s)
+        hashing = {'a':-1,'b':-1,'c':-1} 
+
+        for i in range(n):
+            hashing[s[i]] = i
+            print(hashing)
+        
+            if -1 not in hashing.values():
+
+                cnt += 1 + min(hashing.values())
+
+        return cnt 
