6320a9b97d9964267799cc447a859623313c891f
sheriff18 <135992427+sheriff18@users.noreply.github.com>
2024-07-13 12:46:31 -0400

    Time: 617 ms (42.87%) | Memory: 51 MB (13.86%) - LeetSync

--- 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.py
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.py
@@ -6,20 +6,13 @@ class Solution:
             return None  # Handle empty or single-node list
 
         # Single-pass deletion with two pointers (fast and slow)
-        fast, slow = head, head
-        prev = None
-
+        fast, slow,prev = head, head, None
+        
         while fast and fast.next:
             prev = slow
             slow = slow.next
             fast = fast.next.next
-
-        # # Handle lists with even number of nodes
-        # if fast:  # If fast reaches the end (even number of nodes)
-        #     return head
-
-        # Remove middle node considering duplicates (modify prev's next pointer)
-        if prev:
-            prev.next = slow.next
-
+        
+        prev.next = slow.next
         return head
+        
