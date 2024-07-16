566ce6e08237111854d1e97acafc7f40c0ba8681
sheriff18 <135992427+sheriff18@users.noreply.github.com>
2024-07-16 14:53:37 -0400

    Time: 46 ms (41.29%) | Memory: 19.2 MB (28.09%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.py
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.py
@@ -6,26 +6,16 @@
 
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        def get_meeting_point(head):
-            fast = slow = head
-            while fast and fast.next:
-                fast = fast.next.next
-                slow = slow.next
-                if slow == fast:
-                    return slow
-            return None
-        
-        meeting_point = get_meeting_point(head)
-        if not meeting_point:
-            return None
-
-        slow = head
-        fast = meeting_point
-
-        while slow != fast:
-            fast = fast.next
+        fast = slow = head
+        while fast and fast.next:
+            fast = fast.next.next
             slow = slow.next
-        return fast
-
-
-
+            if slow == fast:
+                slow = head
+                while slow != fast:
+                    slow = slow.next
+                    fast = fast.next
+                return fast
+        return None
+        
+        
