fa6ea050880bb6e00262cd5b6080c70d53404f99
sheriff18 <135992427+sheriff18@users.noreply.github.com>
2024-09-12 10:41:28 -0400

    Time: 54 ms (24.05%) | Memory: 17.2 MB (37.35%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -4,18 +4,13 @@ class Solution:
         left, right = 0, len(nums) - 1
 
         while left <= right:
-            mid = (left + right) // 2
-            if nums[mid] > target:
-                right = mid - 1
-            elif nums[mid] < target:
+            mid = left + (right - left) // 2
+
+            if nums[mid] < target:
                 left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
             else:
                 return mid
             
         return left
-
-       
-
-        
-            
-        
